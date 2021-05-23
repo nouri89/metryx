9d496a6e95704f4ce508f69d662cb13614fc8ac0
nouri89 <nouri89@hotmail.com>
2021-05-23 20:31:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./components/BarChart";
+import BarChart from "./components/BarChart";
+
+function App() {
+	return (
+		<div className="App">
+			<BarChart />
+		</div>
+	);
+}
+
+export default App;
